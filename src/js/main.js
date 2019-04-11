cdc35ad3eb448524078e76d2f80a47c0e747c6ac
russellgoldenberg <russellgoldenberg@gmail.com>
2019-04-11 14:57:54 -0400

    timing

--- src/js/main.js
+++ src/js/main.js
@@ -14,12 +14,14 @@ import Outro from './section-outro';
 const $main = d3.select('main');
 
 async function runAll() {
+  console.time('run');
   await Intro.run();
   await Refresher.run();
   await Average.run();
   await Time.run();
   await Report.run();
   await Outro.run();
+  console.timeEnd('run');
 }
 
 function setupSlide() {
