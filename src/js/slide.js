0259f39e3428808ac96698473cf226f9a9c0982f
russellgoldenberg <russellgoldenberg@gmail.com>
2019-04-24 15:53:31 -0400

    some padding to enter/exit

--- src/js/slide.js
+++ src/js/slide.js
@@ -1,7 +1,8 @@
 /* global d3, WIDTH, HEIGHT, FONT_SIZE */
 export default function slide({ sel, state, dur = 500 }) {
   return new Promise(resolve => {
-    const e = state === 'enter' ? d3.easeCubicOut : d3.easeCubicIn;
+    const ease = state === 'enter' ? d3.easeCubicOut : d3.easeCubicIn;
+    const delay = state === 'enter' ? 250 : 0;
     let x = 0;
     let y = 0;
     if (state === 'exit') x = -WIDTH;
@@ -10,8 +11,11 @@ export default function slide({ sel, state, dur = 500 }) {
     sel
       .transition()
       .duration(dur)
-      .ease(e)
+      .delay(delay)
+      .ease(ease)
       .style('transform', `translate(${x}px, ${y}px)`)
-      .on('end', resolve);
+      .on('end', () => {
+        d3.timeout(resolve, state === 'exit' ? 250 : 0);
+      });
   });
 }
